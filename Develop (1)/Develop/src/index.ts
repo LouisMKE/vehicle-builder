c247c08749a8c10aa3ad4770b3b6257da52ed01d
ljc <ljcary24@gmail.com>
2024-09-09 02:57:17 -0500

    Finished index.ts code, Created and finished README.md file, and recorded/linked demo video

--- Develop (1)/Develop/src/index.ts
+++ Develop (1)/Develop/src/index.ts
@@ -19,23 +19,40 @@ class Vehicle {
 
 // Car class extending Vehicle
 class Car extends Vehicle {
-  constructor(make: string, model: string, year: number) {
+  fuelType: string;
+  numberOfDoors: number;
+
+  constructor(make: string, model: string, year: number, fuelType: string, numberOfDoors: number) {
     super(make, model, year);
+    this.fuelType = fuelType;
+    this.numberOfDoors = numberOfDoors;
+  }
+
+  override displayInfo() {
+    super.displayInfo();
+    console.log(`Fuel Type: ${this.fuelType}`);
+    console.log(`Number of Doors: ${this.numberOfDoors}`);
   }
 }
 
 // Truck class extending Vehicle
 class Truck extends Vehicle {
   loadCapacity: number;
+  towingCapacity: number;
+  numberOfAxles: number;
 
-  constructor(make: string, model: string, year: number, loadCapacity: number) {
+  constructor(make: string, model: string, year: number, loadCapacity: number, towingCapacity: number, numberOfAxles: number) {
     super(make, model, year);
     this.loadCapacity = loadCapacity;
+    this.towingCapacity = towingCapacity;
+    this.numberOfAxles = numberOfAxles;
   }
 
-  override displayInfo() {  // Add the 'override' keyword to resolve TS4114
+  override displayInfo() {
     super.displayInfo();
     console.log(`Load Capacity: ${this.loadCapacity} kg`);
+    console.log(`Towing Capacity: ${this.towingCapacity} kg`);
+    console.log(`Number of Axles: ${this.numberOfAxles}`);
   }
 
   loadCargo(weight: number) {
@@ -50,16 +67,49 @@ class Truck extends Vehicle {
 // Motorbike class extending Vehicle
 class Motorbike extends Vehicle {
   engineType: string;
+  hasSidecar: boolean;
+  topSpeed: number;
 
-  constructor(make: string, model: string, year: number, engineType: string) {
+  constructor(make: string, model: string, year: number, engineType: string, hasSidecar: boolean, topSpeed: number) {
     super(make, model, year);
     this.engineType = engineType;
+    this.hasSidecar = hasSidecar;
+    this.topSpeed = topSpeed;
   }
 
-  override displayInfo() {  // Add the 'override' keyword to resolve TS4114
+  override displayInfo() {
     super.displayInfo();
     console.log(`Engine Type: ${this.engineType}`);
+    console.log(`Has Sidecar: ${this.hasSidecar ? 'Yes' : 'No'}`);
+    console.log(`Top Speed: ${this.topSpeed} km/h`);
   }
+}
+
+// Store for vehicles
+const vehicles: Vehicle[] = [];
+
+// Function to select an existing vehicle
+async function selectExistingVehicle() {
+  if (vehicles.length === 0) {
+    console.log("No vehicles available. Please create one first.");
+    return;
+  }
+
+  const vehicleChoices = vehicles.map((vehicle, index) => ({
+    name: `${vehicle.constructor.name} - ${vehicle.make} ${vehicle.model}`,
+    value: index
+  }));
+
+  const { selectedVehicleIndex } = await inquirer.prompt([
+    {
+      type: 'list',
+      name: 'selectedVehicleIndex',
+      message: 'Select an existing vehicle:',
+      choices: vehicleChoices,
+    }
+  ]);
+
+  return vehicles[selectedVehicleIndex];
 }
 
 // Function to create a vehicle using Inquirer prompts
@@ -73,45 +123,112 @@ async function createVehicle() {
     },
   ]);
 
-  if (vehicleType === 'Truck') {
-    const truckDetails = await inquirer.prompt([
-      { type: 'input', name: 'make', message: 'Truck Make:' },
-      { type: 'input', name: 'model', message: 'Truck Model:' },
-      { type: 'input', name: 'year', message: 'Truck Year:' },
-      { type: 'input', name: 'loadCapacity', message: 'Load Capacity (in kg):' },
-    ]);
+  let vehicle: Vehicle | undefined;
 
-    const truck = new Truck(truckDetails.make, truckDetails.model, +truckDetails.year, +truckDetails.loadCapacity);
-    truck.displayInfo();
+  switch (vehicleType) {
+    case 'Truck':
+      const truckDetails = await inquirer.prompt([
+        { type: 'input', name: 'make', message: 'Truck Make:' },
+        { type: 'input', name: 'model', message: 'Truck Model:' },
+        { type: 'input', name: 'year', message: 'Truck Year:' },
+        { type: 'input', name: 'loadCapacity', message: 'Load Capacity (in kg):' },
+        { type: 'input', name: 'towingCapacity', message: 'Towing Capacity (in kg):' },
+        { type: 'input', name: 'numberOfAxles', message: 'Number of Axles:' },
+      ]);
 
-    // Example of the truck-specific action
-    const { weight } = await inquirer.prompt([
-      { type: 'input', name: 'weight', message: 'How much cargo to load (in kg)?' }
-    ]);
-    truck.loadCargo(+weight);
+      vehicle = new Truck(truckDetails.make, truckDetails.model, +truckDetails.year, +truckDetails.loadCapacity, +truckDetails.towingCapacity, +truckDetails.numberOfAxles);
+      break;
 
-  } else if (vehicleType === 'Motorbike') {
-    const bikeDetails = await inquirer.prompt([
-      { type: 'input', name: 'make', message: 'Motorbike Make:' },
-      { type: 'input', name: 'model', message: 'Motorbike Model:' },
-      { type: 'input', name: 'year', message: 'Motorbike Year:' },
-      { type: 'input', name: 'engineType', message: 'Engine Type:' },
-    ]);
+    case 'Motorbike':
+      const bikeDetails = await inquirer.prompt([
+        { type: 'input', name: 'make', message: 'Motorbike Make:' },
+        { type: 'input', name: 'model', message: 'Motorbike Model:' },
+        { type: 'input', name: 'year', message: 'Motorbike Year:' },
+        { type: 'input', name: 'engineType', message: 'Engine Type:' },
+        { type: 'confirm', name: 'hasSidecar', message: 'Does the motorbike have a sidecar?' },
+        { type: 'input', name: 'topSpeed', message: 'Top Speed (in km/h):' },
+      ]);
 
-    const motorbike = new Motorbike(bikeDetails.make, bikeDetails.model, +bikeDetails.year, bikeDetails.engineType);
-    motorbike.displayInfo();
+      vehicle = new Motorbike(bikeDetails.make, bikeDetails.model, +bikeDetails.year, bikeDetails.engineType, bikeDetails.hasSidecar, +bikeDetails.topSpeed);
+      break;
 
-  } else if (vehicleType === 'Car') {
-    const carDetails = await inquirer.prompt([
-      { type: 'input', name: 'make', message: 'Car Make:' },
-      { type: 'input', name: 'model', message: 'Car Model:' },
-      { type: 'input', name: 'year', message: 'Car Year:' },
-    ]);
+    case 'Car':
+      const carDetails = await inquirer.prompt([
+        { type: 'input', name: 'make', message: 'Car Make:' },
+        { type: 'input', name: 'model', message: 'Car Model:' },
+        { type: 'input', name: 'year', message: 'Car Year:' },
+        { type: 'input', name: 'fuelType', message: 'Fuel Type (e.g., Gas, Electric, Hybrid):' },
+        { type: 'input', name: 'numberOfDoors', message: 'Number of Doors:' },
+      ]);
 
-    const car = new Car(carDetails.make, carDetails.model, +carDetails.year);
-    car.displayInfo();
+      vehicle = new Car(carDetails.make, carDetails.model, +carDetails.year, carDetails.fuelType, +carDetails.numberOfDoors);
+      break;
+  }
+
+  if (vehicle) {
+    vehicles.push(vehicle);
+    return vehicle;
+  } else {
+    throw new Error("Failed to create vehicle");
   }
 }
 
-// Call the createVehicle function to start the vehicle creation process
-createVehicle();
+// Function to perform actions on a vehicle
+async function performActions(vehicle: Vehicle) {
+  let exit = false;
+
+  while (!exit) {
+    const { action } = await inquirer.prompt([
+      {
+        type: 'list',
+        name: 'action',
+        message: 'What would you like to do?',
+        choices: ['Display Info', vehicle instanceof Truck ? 'Load Cargo' : null, 'Exit'].filter(Boolean),
+      }
+    ]);
+
+    if (action === 'Display Info') {
+      vehicle.displayInfo();
+    } else if (action === 'Load Cargo' && vehicle instanceof Truck) {
+      const { weight } = await inquirer.prompt([
+        { type: 'input', name: 'weight', message: 'How much cargo to load (in kg)?' }
+      ]);
+      vehicle.loadCargo(+weight);
+    } else if (action === 'Exit') {
+      exit = true;
+    }
+  }
+}
+
+// Main function to handle user choices
+async function mainMenu() {
+  let exit = false;
+
+  while (!exit) {
+    const { choice } = await inquirer.prompt([
+      {
+        type: 'list',
+        name: 'choice',
+        message: 'Would you like to create a new vehicle or use an existing vehicle?',
+        choices: ['Create New Vehicle', 'Use Existing Vehicle', 'Exit'],
+      },
+    ]);
+
+    let vehicle: Vehicle | undefined;
+
+    if (choice === 'Create New Vehicle') {
+      vehicle = await createVehicle();
+    } else if (choice === 'Use Existing Vehicle') {
+      vehicle = await selectExistingVehicle();
+    } else if (choice === 'Exit') {
+      exit = true;
+    }
+
+    if (vehicle) {
+      await performActions(vehicle);
+    }
+  }
+}
+
+// Start the application
+mainMenu();
--- README.md
+++ README.md
@@ -1,1 +1,48 @@
-# vehicle-builder
+# The Vehicle-Builder Command Line Application
+
+The "Vehicle Builder" command line application allows users to enter the data of a vehicle (Car, Truck, or Motorbike) and recall upon that data at a later time.
+
+# Vehicle-Builder Guide
+Upon running the application, users will be prompted with the question "Would you like to create a new vehicle or use an existing vehicle?"
+
+    If users have previously submitted vehicles to the app, then a dropdown list will appear with the previously saved vehicles for the user to select from. 
+    If the user wishes to create a new vehicle, they must simply select the option "Create New Vehicle", after which, they will be prompted specific questions GEARED towards the type of vehicle they wish to create.
+
+        The questions that will be prompted are as follows in this order:
+                Car:
+                    Car make
+                    Car Model
+                    Car Year
+                    Fuel Type
+                    Number of Doors
+                Truck:
+                    Truck Make
+                    Truck Model
+                    Truck Year
+                    Load Capacity (in kg)
+                    Towing Capacity (in kg)
+                    Number of Axels
+                Motorbike:
+                    Motorbike Make
+                    Motorbike Model
+                    Motorbike Year
+                    Engine Type
+                    Does the motorbike have a sidecar?
+                    Top Speed
+    Note: After the final truck prompt, an option will appear to "Load Cargo". This is a completly optional prompt that is only necessary for those who plan on loading cargo.
+        -If a user wants to sleect that option they will be prompted with "How much cargo do you want to load (in kg)?"
+
+After the final prompt of every vehicle type there will be the option to display the data the user has entered in a clear organized fashion, if you wish to do this click on the "Display Info" prompt.
+Finally, if a user wishes to comb through the list/data of vehicles previously entered they will select "Use Existing Vehicle" when prompted.
+
+Users can exit any time with the "Exit" button displayed at the bottom of the prompt list.
+
+# Future Improvments
+This version of the app only supports those who want to collect data on Cars, Trucks, and motorbikes. Extending the range of vehicle types would be necessary in future updates.
+Additionaly the prompts for each vehicle type are limited. More prompts, as well as a place for users to leave self-written notes for each vehicle is an improvment that can be made.
+
+# Sources
+Note: This project was completed using the help of Xpert Learning Assistant. Specifically in debugging and applying functions that we haven't previously diuscussed in class such as the "switch" and "case" functions.
+
+# Video Link
+Link to Vehicle-Builder Demo: <https://drive.google.com/file/d/1HrlCvSqu2y0S6UsY2q-8OEDuXxLBB4fp/view>
